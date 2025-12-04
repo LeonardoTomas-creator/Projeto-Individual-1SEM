853761a399d789e2a84246765f03b2ce717913eb
LeonardoTomas-creator <leonardo.fsilva@sptech.school>
2025-12-03 21:46:14 -0300

    Update jogo.js - retirando mosquitos

--- public/jogo-grammy/jogo.js
+++ public/jogo-grammy/jogo.js
@@ -3,18 +3,18 @@ var altura = 0;
 var largura = 0;
 var vidas = 1;
 var tempo = 10;
-var criaMosquitoTempo = 1500;
+var criaRostoKatyTempo = 1500;
 
 // ----- NÍVEL DE DIFICULDADE -----
 var nivel = window.location.search;
 nivel = nivel.replace("?", "");
 
 if (nivel == "normal") {
-  criaMosquitoTempo = 1500;
+  criaRostoKatyTempo = 1500;
 } else if (nivel == "dificil") {
-  criaMosquitoTempo = 1000;
+  criaRostoKatyTempo = 1000;
 } else if (nivel == "beyonce") {
-  criaMosquitoTempo = 750;
+  criaRostoKatyTempo = 750;
 }
 
 // ----- AJUSTA TAMANHO DO PALCO -----
@@ -41,9 +41,9 @@ var cronometro = setInterval(function () {
 
 // ----- POSIÇÃO RANDÔMICA -----
 function posicaoRandomica() {
-  // 1 - Remove o mosquito anterior (caso exista).
-  if (document.getElementById("mosquito")) {
-    document.getElementById("mosquito").remove();
+  // 1 - Remove o rosto anterior (caso exista).
+  if (document.getElementById("RostoKaty")) {
+    document.getElementById("RostoKaty").remove();
 
     // 2 - Controla as vidas
     if (vidas > 3) {
@@ -72,24 +72,24 @@ function posicaoRandomica() {
 
   console.log(posicaoX, posicaoY);
 
-  // 4 - Cria o mosquito
+  // 4 - Cria o rosto
   // cria o elemento html
-  // cria dinamicamente um <img> com o mosquito em uma posição aleatória.
-  var mosquito = document.createElement("img");
-  mosquito.src = "imagens/katy.png";
-  mosquito.className = tamanhoAleatorio() + " " + ladoAleatorio();
-  mosquito.style.left = posicaoX + "px";
-  mosquito.style.top = posicaoY + "px";
-  mosquito.style.position = "absolute";
-  mosquito.id = "mosquito";
+  // cria dinamicamente um <img> com o rosto em uma posição aleatória.
+  var RostoKaty = document.createElement("img");
+  RostoKaty.src = "imagens/katy.png";
+  RostoKaty.className = tamanhoAleatorio() + " " + ladoAleatorio();
+  RostoKaty.style.left = posicaoX + "px";
+  RostoKaty.style.top = posicaoY + "px";
+  RostoKaty.style.position = "absolute";
+  RostoKaty.id = "RostoKaty";
 
   // 5 - Evento de clique
-  mosquito.onclick = function () {
+  RostoKaty.onclick = function () {
     this.remove();
   };
 
-  // 6 - Adiciona na tela () o mosquito aparece no corpo da página)
-  document.body.appendChild(mosquito);
+  // 6 - Adiciona na tela () o rosto aparece no corpo da página)
+  document.body.appendChild(RostoKaty);
 }
 
 // ----- POSIÇÃO RANDÔMICA TAMANHO IMAGEM -----
